9164446e7a68c763c14116d6a3440762d423f1d6
ilyesschagaar <i.chagaar@gmail.com>
2021-09-14 10:57:32 +0200

    add modele

---
+++ modele.php
@@ -0,0 +1,20 @@
+<?php
+// On démarre une session
+session_start();
+
+// On inclut la connexion à la base
+require_once('connect.php');
+
+$sql = 'SELECT * FROM `liste`';
+
+// On prépare la requête
+$query = $db->prepare($sql);
+
+// On exécute la requête
+$query->execute();
+
+// On stocke le résultat dans un tableau associatif
+$result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+require_once('close.php');
+?>
